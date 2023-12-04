e13a72f2d68d22d4e9eb4e15d2c58d388a9dbfda
Geri Ramadani <ramadanigeri@gmail.com>
2023-12-04 16:00:58 +0100

    Add Task Delete Feature

---
+++ MyTaskManagement.Application/MyTasks/Commands/DeleteTask/DeleteMyTaskCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManagement.Application.MyTasks.Commands.DeleteTask
+{
+    public class DeleteMyTaskCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ MyTaskManagement.Application/MyTasks/Commands/DeleteTask/DeleteMyTaskCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using MyTaskManagement.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManagement.Application.MyTasks.Commands.DeleteTask
+{
+    public class DeleteMyTaskCommandHandler : IRequestHandler<DeleteMyTaskCommand, int>
+    {
+        private readonly IMyTaskRepository _myTaskRepository;
+
+        public DeleteMyTaskCommandHandler(IMyTaskRepository myTaskRepository) 
+        {
+            _myTaskRepository = myTaskRepository;
+        }
+        public async Task<int> Handle(DeleteMyTaskCommand request, CancellationToken cancellationToken)
+        {
+            return await _myTaskRepository.DeleteTask(request.Id);
+        }
+    }
+}
