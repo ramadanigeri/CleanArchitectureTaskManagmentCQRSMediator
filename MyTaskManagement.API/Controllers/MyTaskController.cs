8c8e7b49fd4b6cec31ce9db0797bf4c68a2a1f20
Geri Ramadani <ramadanigeri@gmail.com>
2023-12-04 17:48:20 +0100

    Update Presentation Layer with GetTask & GetTaskById + Code Cleanup

---
+++ MyTaskManagement.API/Controllers/MyTaskController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyTaskManagement.Application.MyTasks.Queries.GetTasks;
+using MyTaskManagement.Application.MyTasks.Queries.GetTasksById;
+using MyTaskManagement.Domain.Entity;
+
+namespace MyTaskManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyTaskController : APIControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllTasks()
+        {
+            var myTask = await Mediator.Send(new GetMyTaskQuery());
+            return Ok(myTask);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTaskById(int id)
+        {
+            var myTask = await Mediator.Send(new GetMyTaskByIdQuery() { MyTaskId = id });
+            if(myTask == null)
+            {
+                return NotFound();
+            }
+            return Ok(myTask);
+        }
+    }
+}
