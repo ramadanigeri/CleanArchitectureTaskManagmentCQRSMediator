cc4240a0885d6569c29ed0eafacdf732e042f3dd
Geri Ramadani <ramadanigeri@gmail.com>
2023-12-04 15:49:04 +0100

    Add Update Task feature

---
+++ MyTaskManagement.Application/MyTasks/Commands/UpdateTask/UpdateMyTaskCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManagement.Application.MyTasks.Commands.UpdateTask
+{
+    public class UpdateMyTaskCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public string? Category { get; set; }
+
+        public string? CreatedBy { get; set; }
+    }
+}
---
+++ MyTaskManagement.Application/MyTasks/Commands/UpdateTask/UpdateMyTaskCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using MyTaskManagement.Domain.Entity;
+using MyTaskManagement.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManagement.Application.MyTasks.Commands.UpdateTask
+{
+    public class UpdateMyTaskCommandHandler : IRequestHandler<UpdateMyTaskCommand, int>
+    {
+        private readonly IMyTaskRepository _myTaskRepository;
+
+        public UpdateMyTaskCommandHandler(IMyTaskRepository myTaskRepository) 
+        {
+            _myTaskRepository = myTaskRepository;
+        }
+        public async Task<int> Handle(UpdateMyTaskCommand request, CancellationToken cancellationToken)
+        {
+            var updateMyTaskEntity = new MyTask()
+            {
+                Id = request.Id,
+                Name = request.Name,
+                Description = request.Description,
+                Category = request.Category,
+                CreatedBy = request.CreatedBy,
+            };
+            return await _myTaskRepository.UpdateTask(request.Id, updateMyTaskEntity);
+        }
+    }
+}
