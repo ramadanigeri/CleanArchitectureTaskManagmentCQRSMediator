b83a44f1504b00ecdb202ff5031842c31d7c13e5
Geri Ramadani Local <ramadanigeri@gmail.com>
2023-12-04 12:00:45 +0100

    Add project files.

---
+++ MyTaskManagement.API/Program.cs
@@ -0,0 +1,28 @@
+using MyTaskManagement.Application;
+using MyTaskManagement.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Layers dependency
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ MyTaskManagement.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManagement.Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
