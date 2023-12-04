9059bdfd59e6faecd8d94fb6dea6fbce6981e85a
Geri Ramadani <ramadanigeri@gmail.com>
2023-12-04 14:01:01 +0100

    Update Domain Layer with entities and repository

---
+++ MyTaskManagement.Domain/Entity/MyTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManagement.Domain.Entity
+{
+    public class MyTask
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string Category { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ MyTaskManagement.Domain/Repository/IMyTaskRepository.cs
@@ -0,0 +1,18 @@
+﻿using MyTaskManagement.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManagement.Domain.Repository
+{
+    public interface IMyTaskRepository
+    {
+        Task<List<MyTask>> GetAllTasks();
+        Task<MyTask> GetTaskById(int id);
+        Task<MyTask> CreateTask(MyTask myTask);
+        Task<int> UpdateTask(int id, MyTask myTask);
+        Task<int> DeleteTask(int id);
+    }
+}
