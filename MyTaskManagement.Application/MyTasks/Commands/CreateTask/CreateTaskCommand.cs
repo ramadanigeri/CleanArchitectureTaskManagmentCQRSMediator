e6377e5047ada42d6aeefbfdad7296287ebbaa47
Geri Ramadani <ramadanigeri@gmail.com>
2023-12-04 15:39:11 +0100

    Add Create new task feature

---
+++ MyTaskManagement.Application/MyTasks/Commands/CreateTask/CreateTaskCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using MyTaskManagement.Application.MyTasks.Queries.GetTasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManagement.Application.MyTasks.Commands.CreateTask
+{
+    public class CreateTaskCommand : IRequest<MyTaskVM>
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+        public string? CreatedBy { get; set; }
+    }
+}
---
+++ MyTaskManagement.Application/MyTasks/Commands/CreateTask/CreateTaskCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using MyTaskManagement.Application.MyTasks.Queries.GetTasks;
+using MyTaskManagement.Domain.Entity;
+using MyTaskManagement.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManagement.Application.MyTasks.Commands.CreateTask
+{
+    public class CreateTaskCommandHandler : IRequestHandler<CreateTaskCommand, MyTaskVM>
+    {
+        private readonly IMyTaskRepository _myTaskRepository;
+        private readonly IMapper _mapper;
+
+        public CreateTaskCommandHandler(IMyTaskRepository myTaskRepository, IMapper mapper) 
+        {
+            _myTaskRepository = myTaskRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<MyTaskVM> Handle(CreateTaskCommand request, CancellationToken cancellationToken)
+        {
+            var taskEntity = new MyTask() {  Name = request.Name, Description = request.Description, Category = request.Category, CreatedBy = request.CreatedBy };
+            var result = await _myTaskRepository.CreateTask(taskEntity);
+            return _mapper.Map<MyTaskVM>(result);
+
+        }
+    }
+}
